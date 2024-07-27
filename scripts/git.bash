3f4798154601651f5ab7ffd9a620b1de1a1427d3
Musah Ibrahim Ali <softwarearomagh@gmail.com>
2024-07-27 21:01:43 +0000

    Adding new files and updating the existing ones

--- scripts/git.bash
+++ scripts/git.bash
@@ -1,7 +1,13 @@
 # add a bash code to add all files to git and commit them with a message and push
 # the changes to the remote repository
 # the script should take the commit message as an argument
-message = "Adding new files and updating the existing ones"
+msg = $1
+# add a message if the user does not provide one
+if [ -z "$msg" ]; then
+    message="Adding new files and updating the existing ones"
+else
+    message=$msg
+fi
 git add .
 git commit -m "$message"
 git push
